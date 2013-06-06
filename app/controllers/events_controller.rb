0b8c8bd40e75a83ac29d8eeae0a1a4650a36fc65
yuta takahashi <m1205123ing@gmail.com>
2013-06-06 17:18:12 +0900

    implement the function of Attendance registration.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :set_event, :set_events_user, only: [:show, :edit, :update, :destroy]
 
   # GET /events
   # GET /events.json
@@ -10,6 +10,7 @@ class EventsController < ApplicationController
   # GET /events/1
   # GET /events/1.json
   def show
+    @attend = ['未回答', '出席', '欠席', '未定']
   end
 
   # GET /events/new
@@ -65,6 +66,11 @@ class EventsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_event
       @event = Event.find(params[:id])
+    end
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_events_user
+      @events_users = EventsUser.where(:event_id => params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
