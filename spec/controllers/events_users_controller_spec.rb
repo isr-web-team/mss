0a48f9eb8c38922c8cae22b55f024ded70456fd0
Y-Fujikawa <fujikawa0@gmail.com>
2013-06-09 22:41:40 +0900

    自動テストの作成 1. コントローラーのテストクラスを作成 2. ページにアクセスした場合のテストケースを追加（events_usersはひな形のみ）

---
+++ spec/controllers/events_users_controller_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+describe EventsUsersController do
+end
---
+++ spec/controllers/top_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe TopController do
+  describe 'index' do
+    context '一覧ページにアクセスした場合' do
+      before do
+        get :index
+      end
+      
+      it 'HTTPレスポンスが 200系を返す' do
+        response.should be_success
+      end
+      
+      it 'index のテンプレートが読まれる' do
+        response.should render_template('index')
+      end
+    end
+  end
+end
