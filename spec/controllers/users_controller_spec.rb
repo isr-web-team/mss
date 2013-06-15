e8863306c2339b7b3a535e8acc070b79fc7166e6
Yasuyuki Fujikawa <fujikawa0@gmail.com>
2013-06-15 13:08:28 +0900

    自動テストの作成 1. RSpecの書き方に習ってメソッド名を修正 2. users_controllerクラスのeditメソッドのテストケースを追加

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,18 +3,18 @@ require 'spec_helper'
 describe UsersController do
   fixtures :users
   
-  describe 'index' do
-    context 'indexにアクセスした場合' do
+  describe 'usersページのindex' do
+    context 'アクセスに成功' do
       before do
         get :index
       end
       
       it 'HTTPレスポンスが 200系を返す' do
-        response.should be_success
+        expect(response).to be_success
       end
       
       it 'index のテンプレートが読まれる' do
-        response.should render_template('index')
+        expect(response).to render_template('index')
       end
       
       it '@users に詳細データが格納されている' do
@@ -23,18 +23,18 @@ describe UsersController do
     end
   end
   
-  describe 'show' do
+  describe 'usersページのshow' do
     context '存在するIDにアクセス' do
       before do
         get :show, :id => 1
       end
       
       it 'HTTPレスポンスが 200系を返す' do
-        response.should be_success
+        expect(response).to be_success
       end
       
       it 'show のテンプレートが読まれる' do
-        response.should render_template('show')
+        expect(response).to render_template('show')
       end
     end
     
@@ -48,22 +48,47 @@ describe UsersController do
     end
   end
   
-  describe 'new' do
-    context 'Userの新規作成' do
+  describe 'usersページのnew' do
+    context 'アクセスに成功' do
       before do
         get :new
       end
       
       it 'HTTPレスポンスが 200系を返す' do
-        response.should be_success
+        expect(response).to be_success
       end
       
       it 'show のテンプレートが読まれる' do
-        response.should render_template('new')
+        expect(response).to render_template('new')
       end
       
       it 'インスタンスが生成される(中身は空)' do
         assigns[:users].should == nil
+      end
+    end
+  end
+  
+  describe 'usersページのedit' do
+    context 'アクセスに成功' do
+      before do
+        get :edit, :id => 1
+      end
+      
+      it 'HTTPレスポンスが 200系を返す' do
+        expect(response).to be_success
+      end
+      
+      it 'edit のテンプレートが読まれる' do
+        expect(response).to render_template('edit')
+      end
+    end
+    
+    context '存在しないIDにアクセス' do
+      before do
+      end
+      
+      it 'RecordNotFound が返却される' do
+        expect { User.find(99) }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
   end
