2308d945139c396118ad0798c51e9b382d558d9e
Yasuyuki Fujikawa <fujikawa0@gmail.com>
2013-06-19 23:17:34 +0900

    #10 login_id, passwordのvalidateを実装 ※画面表示は未実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :events_users
   has_many :events, :through => :users
+  
+  validates :login_id, :presence => { :message => 'ログインIDは必ず入力してください。'}
+  # validates :name, :presence => { :message => '名前は必ず入力してください。'}
+  # validates :email, :presence => { :message => 'メールアドレスは必ず入力してください。'}
+  validates :password, :confirmation => { :message => 'パスワードが違います。'}
 end
