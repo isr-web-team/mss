8b2eee59afa71a8e2d76c80d410ac33a7c2dd68e
Yasuyuki Fujikawa <fujikawa0@gmail.com>
2013-06-22 10:16:12 +0900

    #10 passwordの画面表示部分を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
   validates :login_id, :presence => { :message => 'ログインIDは必ず入力してください。'}
   # validates :name, :presence => { :message => '名前は必ず入力してください。'}
   # validates :email, :presence => { :message => 'メールアドレスは必ず入力してください。'}
-  validates :password, :confirmation => { :message => 'パスワードが違います。'}
+  validates :password, :confirmation => { :message => 'パスワードと確認用パスワードが違います。'}
 end
