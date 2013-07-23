a2b1649adda80da63172540c145e097c5ef0e319
yuta takahashi <m1205123ing@gmail.com>
2013-07-23 23:32:17 +0900

    add validates at create attend.

--- app/models/events_user.rb
+++ app/models/events_user.rb
@@ -2,4 +2,6 @@ class EventsUser < ActiveRecord::Base
   belongs_to :event
   belongs_to :user
   primary_keys = :user_id, :event_id
+
+  validates :user_id, :presence => { :message => 'ユーザーは必ず入力してください。'}
 end
